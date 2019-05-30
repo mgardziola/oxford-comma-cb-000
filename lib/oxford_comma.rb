5b58e803d94184da024b1667dcf4c1a51851014e
mariah <mariah@gardziola.com>
2019-05-30 18:32:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  
+  case array.length 
+  when 0 
+    ''
+  when 1 
+    array.first 
+  when 2
+    "#{array[0]} and #{array[1]}"
+  else 
+    "#{array[0...-1].join(", ")}, and #{array[-1]}"
+  end 
 
 end
